a296ddcce2a95027daad9f6efa1e2747a46c7c15
Alexander Kriegisch <Alexander@Kriegisch.name>
2022-01-28 10:25:42 +0700

    Simple Maven project with test and action depending on input variable

---
+++ src/test/java/org/example/DummyTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DummyTest {
+  @Test
+  public void testSomething() {
+    assertTrue(true);
+  }
+}
